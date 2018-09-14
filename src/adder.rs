707218e71def7d2522427ea040f4147f259ce0a8
Josh <josh@elem14.tech>
2018-09-14 02:34:34 -0500

    Adds all numbers.

---
+++ src/adder.rs
@@ -0,0 +1,8 @@
+pub fn adder(numbers: &mut Vec<u64>) -> u128 {
+    let mut number_holder: u128 = 0;
+
+    for &item in numbers.iter() {
+        number_holder += item as u128;
+    }
+    number_holder
+}
