354bf920caca7f54e3f16d6f6c7337e1ec198f6a
Josh <josh@elem14.tech>
2018-09-14 02:35:30 -0500

    Added more mathematics.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,36 @@ extern crate rand;
 
 mod input;
 mod largest;
+mod smallest;
+mod adder;
 
 use input::*;
 use largest::*;
+use smallest::*;
+use adder::*;
 use rand::Rng;
 
 fn main() {
-    let mut numbers: Vec<usize> = Vec::new();
+    let mut numbers: Vec<u64> = Vec::new();
     println!("How many numbers should I randomly generate?");
     let user_input = input();
 
     for _ in 0..user_input {
         numbers.push(rand::thread_rng().gen_range(0, 100000000000001));
     }
+    println!("Finding largest number.");
+
     let largest_number = largest(&numbers);
 
-    println!("The largest number in the list you entered is: {}.", largest_number);
+    println!("Finding smallest number.");
+
+    let smallest_number = smallest(&numbers);
+
+    println!("Adding all the numbers up.");
+
+    let added_number = adder(&mut numbers);
+
+    println!("The largest number in the list: {}.", largest_number);
+    println!("The smallest number in the list is: {}", smallest_number);
+    println!("All the numbers added up is: {}", added_number);
 }
